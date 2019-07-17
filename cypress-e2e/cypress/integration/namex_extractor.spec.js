ba98f93d2cde6559c23e77bc0eff55eb8a421939
john_lane <john.a.m.lane@gov.bc.ca>
2019-07-17 13:28:06 -0700

    refactored the extractor tests to resist slow downs

--- cypress-e2e/cypress/integration/namex_extractor.spec.js
+++ cypress-e2e/cypress/integration/namex_extractor.spec.js
@@ -2,62 +2,68 @@ describe('Namex extractor test steps', function () {
     beforeEach(function () {
         cy.getSessionToken();
         cy.get('a[href="/find"]').click();
+        cy.fixture('name-choices').as('names');
     });
 
     it('Verifies that the firm NR was extracted', function () {
-        cy.fixture('name-choices').then((json) => {
-            cy.get('#search-filter-company').type(json.firmname);
-        });
-
+        
+        cy.get('#search-filter-company').type(this.names.firmname);    
         cy.get('#search-filter-submittedDate').select('Today');
+        cy.server();
+        cy.route('**/requests**').as('FindNR');      
         cy.get('#search-filter-state').select('DRAFT');
+        cy.wait('@FindNR');        
+        
+        cy.get('#loading-overlay > div').should('not.be.visible');
 
-        cy.fixture('name-choices').then((json) => {
-            cy.get('#search-table > tbody > tr:nth-child(1) > td:nth-child(4)')
-                .contains(json.firmname);
-        });
+        cy.get('#search-table > tbody > tr:nth-child(1) > td:nth-child(4)')
+            .contains(this.names.firmname);
+    
     });
 
     it('Verifies that the NWPTA NR was extracted', function () {
-        cy.fixture('name-choices').then((json) => {
-            cy.get('#search-filter-company').type(json.NWPTAname);
-        });
-
+                
+        cy.get('#search-filter-company').type(this.names.NWPTAname);    
         cy.get('#search-filter-submittedDate').select('Today');
+        cy.server();
+        cy.route('**/requests**').as('FindNR');
         cy.get('#search-filter-state').select('DRAFT');
+        cy.wait('@FindNR');        
 
-        cy.fixture('name-choices').then((json) => {
-            cy.get('#search-table > tbody > tr:nth-child(1) > td:nth-child(4)')
-                .contains(json.NWPTAname);
-        });
+        cy.get('#loading-overlay > div').should('not.be.visible');
+
+        cy.get('#search-table > tbody > tr:nth-child(1) > td:nth-child(4)')
+            .contains(this.names.NWPTAname);
+
     });
 
     it('Verifies that the extraprovincial NR was extracted', function () {
-        cy.fixture('name-choices').then((json) => {
-            cy.get('#search-filter-company').type(json.EPname);
-        });
-
+                
+        cy.get('#search-filter-company').type(this.names.EPname);    
         cy.get('#search-filter-submittedDate').select('Today');
+        cy.server();
+        cy.route('**/requests**').as('FindNR');
         cy.get('#search-filter-state').select('DRAFT');
+        cy.wait('@FindNR');
+        
+        cy.get('#loading-overlay > div').should('not.be.visible');
 
-        cy.fixture('name-choices').then((json) => {
-            cy.get('#search-table > tbody > tr:nth-child(1) > td:nth-child(4)')
-                .contains(json.EPname);
-        });
+        cy.get('#search-table > tbody > tr:nth-child(1) > td:nth-child(4)')
+            .contains(this.names.EPname);
     });
-
     
     it('Verifies that the LLP NR was extracted', function () {
-        cy.fixture('name-choices').then((json) => {
-            cy.get('#search-filter-company').type(json.LLPname);
-        });
-
+                
+        cy.get('#search-filter-company').type(this.names.LLPname);    
         cy.get('#search-filter-submittedDate').select('Today');
+        cy.server();
+        cy.route('**/requests**').as('FindNR');
         cy.get('#search-filter-state').select('DRAFT');
+        cy.wait('@FindNR');
+        
+        cy.get('#loading-overlay > div').should('not.be.visible');
 
-        cy.fixture('name-choices').then((json) => {
-            cy.get('#search-table > tbody > tr:nth-child(1) > td:nth-child(4)')
-                .contains(json.LLPname);
-        });
+        cy.get('#search-table > tbody > tr:nth-child(1) > td:nth-child(4)')
+            .contains(this.names.LLPname);
     });
 });
